86b8afcff4075456744e3877c4ed5fc14517917e
michalziaja <michalziaja88@gmail.com>
2024-02-11 21:46:32 +0100

    push backend code

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const baseURL = 'http://127.0.0.1:8000/api/v1/' //LOCAL
+const baseURL = 'http://192.168.22.24:8000/api/v1/' //LOCAL
 
-//const baseURL = 'http://fastapi:8000/api/v1/' //Docker
+//const baseURL = 'http://fastapi:8000/api/v1/' //Docker 
 
 const axiosInstance = axios.create({
     baseURL
