de91f3fecfaf33953adb79020f3c0f68fe2fbfbf
michalziaja <michalziaja88@gmail.com>
2024-02-14 12:08:59 +0100

    Update App.test.jsx

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,11 +1,32 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll, beforeEach } from 'vitest';
+const assert = require('assert');
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
+describe("Combined Test Suites", () => {
+  beforeAll(() => {
+    console.log("This part executes once before all tests");
   });
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
+  describe("Simple Calculations", () => {
+    beforeEach(() => {
+      console.log("executes before every test");
+    });
+
+    it("Is returning 5 when adding 2 + 3", () => {
+      assert.equal(2 + 3, 5);
+    });
+
+    it("Is returning 6 when multiplying 2 * 3", () => {
+      assert.equal(2 * 3, 6);
+    });
+  });
+
+  describe("Something Truthy and Falsy", () => {
+    it("true to be true", () => {
+      expect(true).toBe(true);
+    });
+
+    it("false to be false", () => {
+      expect(false).toBe(false);
+    });
   });
 });
